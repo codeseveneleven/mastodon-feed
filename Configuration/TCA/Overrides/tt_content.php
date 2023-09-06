73353fd403c4ca84e9005af9a22b0ff424f7c4f7
pottmar <p.ottmar@12bis3.de>
2023-09-06 11:27:57 +0200

    fixes for v8

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,26 +1,10 @@
 <?php
+defined('TYPO3_MODE') or die();
 
-/*
- * This file is part of the TYPO3 project.
- * (c) 2022 B-Factor GmbH / 12bis3 / Sudhaus7 / code711.de
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- * The TYPO3 project - inspiring people to share!
- *
- * @copyright 2023 B-Factor GmbH / 12bis3 / Sudhaus7 / https://code711.de/
- */
-
-defined('TYPO3') or die();
-
-(function () {
-
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'MastodonFeed',
-        'Feed',
-        'LLL:EXT:mastodon_feed/Resources/Private/Language/locallang.xlf:tt_content.mastodon_feed'
-    );
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['mastodonfeed_feed'] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('mastodonfeed_feed', 'FILE:EXT:mastodon_feed/Configuration/FlexForms/Feed.xml');
-
-})();
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'MastodonFeed',
+    'Feed',
+    'LLL:EXT:mastodon_feed/Resources/Private/Language/locallang.xlf:tt_content.mastodon_feed'
+);
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['mastodonfeed_feed'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('mastodonfeed_feed', 'FILE:EXT:mastodon_feed/Configuration/FlexForms/Feed.xml');
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,29 +1,13 @@
 <?php
+defined('TYPO3_MODE') or die();
 
-/*
- * This file is part of the TYPO3 project.
- * (c) 2022 B-Factor GmbH / 12bis3 / Sudhaus7 / code711.de
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- * The TYPO3 project - inspiring people to share!
- *
- * @copyright 2023 B-Factor GmbH / 12bis3 / Sudhaus7 / https://code711.de/
- */
-
-defined('TYPO3') or die();
-
-(function () {
-
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'MastodonFeed',
-        'Feed',
-        [
-            \Code711\MastodonFeed\Controller\FeedController::class => 'index',
-        ],
-        [
-            \Code711\MastodonFeed\Controller\FeedController::class => 'index',
-        ]
-    );
-
-})();
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'MastodonFeed',
+    'Feed',
+    [
+        'FeedController' => 'index',
+    ],
+    [
+        'FeedController' => 'index',
+    ]
+);

66b97b4644ffb2c10dfb33ed5b6c146a58a08e23
Stefan Froemken <froemken@gmail.com>
2023-12-01 21:42:24 +0100

    Add entry for newContentElementWizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,13 +13,15 @@ declare(strict_types=1);
  * @copyright 2023 B-Factor GmbH / 12bis3 / Sudhaus7 / https://code711.de/
  */
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use Code711\MastodonFeed\Controller\FeedController;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
 
 (function () {
-
     ExtensionUtility::configurePlugin(
         'MastodonFeed',
         'Feed',
@@ -31,4 +33,10 @@ defined('TYPO3') or die();
         ],
     );
 
+    $typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
+    if (version_compare($typo3Version->getBranch(), '12.0', '<')) {
+        ExtensionManagementUtility::addPageTSConfig(
+            '@import \'EXT:mastodon_feed/Configuration/page.tsconfig\''
+        );
+    }
 })();
